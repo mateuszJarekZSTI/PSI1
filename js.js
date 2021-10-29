c70267726ebac01cf69b88c9a3715d611e28d510
mateuszJarekZSTI <90909167+mateuszJarekZSTI@users.noreply.github.com>
2021-10-29 14:24:08 +0200

    works maybe

--- js.js
+++ js.js
@@ -1,3 +1,7 @@
-var liczba1=document.getElementById("liczba1");
-var liczba2=document.getElementById("liczba2");
-var btn = document.querySelectorAll("button")[0];
+const liczba1=document.getElementById("liczba1");
+const liczba2=document.getElementById("liczba2");
+const btn = document.querySelectorAll("button")[0];
+const p=document.querySelectorAll("p")[0];
+btn.addEventListener(
+    p.innerHTML=parseInt(liczba1)+parseInt(liczba2);
+)
