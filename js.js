acb6687fecaa24ee7a250ed49d69f1a4e7766136
mateuszJarekZSTI <90909167+mateuszJarekZSTI@users.noreply.github.com>
2021-10-29 14:25:25 +0200

    now

--- js.js
+++ js.js
@@ -2,6 +2,9 @@ const liczba1=document.getElementById("liczba1");
 const liczba2=document.getElementById("liczba2");
 const btn = document.querySelectorAll("button")[0];
 const p=document.querySelectorAll("p")[0];
-btn.addEventListener(
+
+function funkcja()
+{
     p.innerHTML=parseInt(liczba1)+parseInt(liczba2);
-)
+}
+btn.addEventListener("onclick",funkcja);
